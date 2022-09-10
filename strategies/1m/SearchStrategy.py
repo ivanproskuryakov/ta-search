cd71e8582d6e38a30a1e7fa22f293c3ce47fcc27
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2022-09-10 05:01:29 +0300

    1m interval strategy - Reject force-sells with negative profit

--- strategies/1m/SearchStrategy.py
+++ strategies/1m/SearchStrategy.py
@@ -1,8 +1,11 @@
 import sys
 import pandas as pd
-import freqtrade.vendor.qtpylib.indicators as qtpylib
+from datetime import datetime
 
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+from freqtrade.persistence import Order, PairLocks, Trade
 from freqtrade.strategy.interface import IStrategy
+
 from search1m import Search1m
 
 
@@ -29,3 +32,15 @@ class SearchStrategy(IStrategy):
     def populate_sell_trend(self, df: pd.DataFrame, metadata: dict) -> pd.DataFrame:
         df.loc[(df['sell'] == 'sell'), 'sell'] = 1
         return df
+
+    # https://www.freqtrade.io/en/stable/strategy-advanced/
+
+    def confirm_trade_exit(self, pair: str, trade: Trade, order_type: str, amount: float,
+                           rate: float, time_in_force: str, exit_reason: str,
+                           current_time: datetime, **kwargs) -> bool:
+        if exit_reason == 'exit_signal' and trade.calc_profit_ratio(rate) < 0:
+            # Reject force-sells with negative profit
+            # This is just a sample, please adjust to your needs
+            # (this does not necessarily make sense, assuming you know when you're force-selling)
+            return False
+        return True
